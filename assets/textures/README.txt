072397e5b99d6a646f5d6b74dcaf120d6381303f
sektant1 <fergabe@pm.me>
2026-04-05 13:40:56 -0300

    few formatting

---
+++ assets/textures/README.txt
@@ -0,0 +1,8 @@
+All textures created by me, Serenity "GoldenThumbs" Skiff, and are available to use under CC0.
+
+These textures were created with a variety of techniques and tools by me and are not derived from any existing works.
+The tools used:
+- Gimp.
+- Krita.
+- Material Maker.
+- Substance Designer.
--- engine/src/platform/window.cpp
+++ engine/src/platform/window.cpp
@@ -5,21 +5,25 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-namespace qp {
+namespace qp
+{
 
-struct Window {
-    GLFWwindow* handle;
-    i32 width;
-    i32 height;
+struct Window
+{
+    GLFWwindow *handle;
+    i32         width;
+    i32         height;
 };
 
 static bool s_glfw_initialized = false;
 
-static void glfw_error_callback(int error, const char* description) {
+static void glfw_error_callback(int error, const char *description)
+{
     LOG_ERROR("GLFW error %d: %s", error, description);
 }
 
-Window* window_create(const WindowConfig& config) {
+Window *window_create(const WindowConfig &config)
+{
     if (!s_glfw_initialized) {
         glfwSetErrorCallback(glfw_error_callback);
         if (!glfwInit()) {
@@ -34,8 +38,7 @@ Window* window_create(const WindowConfig& config) {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
-    GLFWwindow* handle = glfwCreateWindow(
-        config.width, config.height, config.title, nullptr, nullptr);
+    GLFWwindow *handle = glfwCreateWindow(config.width, config.height, config.title, nullptr, nullptr);
 
     if (!handle) {
         LOG_FATAL("Failed to create GLFW window");
@@ -52,20 +55,20 @@ Window* window_create(const WindowConfig& config) {
 
     glfwSwapInterval(config.vsync ? 1 : 0);
 
-    Window* w = new Window();
+    Window *w = new Window();
     w->handle = handle;
-    w->width = config.width;
+    w->width  = config.width;
     w->height = config.height;
 
-    LOG_INFO("Window created: %dx%d - %s", config.width, config.height,
-             config.title);
+    LOG_INFO("Window created: %dx%d - %s", config.width, config.height, config.title);
     LOG_INFO("OpenGL: %s", glGetString(GL_VERSION));
     LOG_INFO("Renderer: %s", glGetString(GL_RENDERER));
 
     return w;
 }
 
-void window_destroy(Window* w) {
+void window_destroy(Window *w)
+{
     if (w) {
         glfwDestroyWindow(w->handle);
         delete w;
@@ -76,24 +79,29 @@ void window_destroy(Window* w) {
     }
 }
 
-bool window_should_close(Window* w) {
+bool window_should_close(Window *w)
+{
     return glfwWindowShouldClose(w->handle);
 }
 
-void window_swap_buffers(Window* w) {
+void window_swap_buffers(Window *w)
+{
     glfwSwapBuffers(w->handle);
 }
 
-void window_get_size(Window* w, i32* width, i32* height) {
+void window_get_size(Window *w, i32 *width, i32 *height)
+{
     glfwGetWindowSize(w->handle, width, height);
 }
 
-void window_get_framebuffer_size(Window* w, i32* width, i32* height) {
+void window_get_framebuffer_size(Window *w, i32 *width, i32 *height)
+{
     glfwGetFramebufferSize(w->handle, width, height);
 }
 
-void* window_get_native_handle(Window* w) {
+void *window_get_native_handle(Window *w)
+{
     return w->handle;
 }
 
-} // namespace qp
+}  // namespace qp
