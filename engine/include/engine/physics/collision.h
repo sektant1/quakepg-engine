4b82cbf8ebf842c268a2b081dcf0624ef5091e73
Gabriel Fernandes <gfe@certi.org.br>
2026-04-05 12:48:46 -0300

    add private assets download on deploy

--- engine/include/engine/physics/collision.h
+++ engine/include/engine/physics/collision.h
@@ -16,9 +16,9 @@ inline AABB aabb_from_center_size(Vec3 center, Vec3 size) {
 }
 
 inline bool aabb_intersects(const AABB& a, const AABB& b) {
-    return (a.min.x <= b.max.x && a.max.x >= b.min.x) &&
-           (a.min.y <= b.max.y && a.max.y >= b.min.y) &&
-           (a.min.z <= b.max.z && a.max.z >= b.min.z);
+    return (a.min.x < b.max.x && a.max.x > b.min.x) &&
+           (a.min.y < b.max.y && a.max.y > b.min.y) &&
+           (a.min.z < b.max.z && a.max.z > b.min.z);
 }
 
 // Slide collision: try to move 'box' by 'velocity', resolving against walls.
